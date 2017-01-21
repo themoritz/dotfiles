a26dd8de77dbab5999c364c224a95408f3c5f77d
Moritz Drexl <mdrexl@fastmail.fm>
2017-01-21 12:42:57 +0100

    init.el cleanup + latest version

--- .emacs.d/personal/init.el
+++ .emacs.d/personal/init.el
@@ -7,69 +7,45 @@
 
 ;;; Code:
 
-;;; Prelude packages
+;;; Prelude packages -----------------------------------------------------------
+ 
 (require 'prelude-helm) ;; Interface for narrowing and search
 (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
-(setq evil-want-C-u-scroll t)
 (require 'prelude-evil)
 
-;;; Programming languages support
+;; Programming languages support
 ;; (require 'prelude-c)
 ;; (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
 ;; (require 'prelude-common-lisp)
 ;; (require 'prelude-css)
-(require 'prelude-emacs-lisp)
+;; (require 'prelude-emacs-lisp) ;; breaks intero-blacklist
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-elixir)
 ;; (require 'prelude-go)
-(require 'prelude-haskell)
-(require 'prelude-js)
+;; (require 'prelude-haskell)    ;; breaks intero-blacklist
+;; (require 'prelude-js)         ;; breaks intero-blacklist
 (require 'prelude-latex)
 ;; (require 'prelude-lisp)
 ;; (require 'prelude-ocaml)
-;; (require 'prelude-org) ;; Org-mode helps you keep to do lists, notes and more
+;; (require 'prelude-org)
 ;; (require 'prelude-perl)
 ;; (require 'prelude-python)
 ;; (require 'prelude-ruby)
 ;; (require 'prelude-scala)
 ;; (require 'prelude-scheme)
 (require 'prelude-shell)
-;; (require 'prelude-scss)
+(require 'prelude-scss)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
 ;; (require 'prelude-xml)
-;; (require 'prelude-yaml)
+(require 'prelude-yaml)
 
-; Bootstrap 'use-package'
+;;; Packages -------------------------------------------------------------------
+
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
-;;; Evil customization:
-
-; Remap Vim's C-] to Emacs' jump-to-tag
-(defun my-jump-to-tag ()
-    (interactive)
-    (evil-emacs-state)
-    (call-interactively (key-binding (kbd "M-.")))
-    (evil-change-to-previous-state (other-buffer))
-    (evil-change-to-previous-state (current-buffer)))
-
-(define-key evil-normal-state-map (kbd "C-]") 'my-jump-to-tag)
-
-(use-package evil-magit :ensure t)
-
-;; Fix path
-(use-package exec-path-from-shell :ensure t)
-(exec-path-from-shell-initialize)
-
-;; Theme
-
-(use-package monokai-theme :ensure t)
-(load-theme 'monokai)
-
-;; Get package installation ready
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -79,16 +55,53 @@
              '("emacs-pe" . "https://emacs-pe.github.io/packages/"))
 (package-initialize)
 
-;;; Modes:
+;;; Evil customization ---------------------------------------------------------
+
+;; New binding for `eval-expression'
+(define-key evil-normal-state-map (kbd "M-;") 'eval-expression)
+
+(setq evil-want-C-u-scroll t)
+
+;; Remap Vim's C-] to Emacs' jump-to-tag
+(defun my-jump-to-tag ()
+    (interactive)
+    (evil-emacs-state)
+    (call-interactively (key-binding (kbd "M-.")))
+    (evil-change-to-previous-state (other-buffer))
+    (evil-change-to-previous-state (current-buffer)))
+(define-key evil-normal-state-map (kbd "C-]") 'my-jump-to-tag)
+
+;; Smart parens
+(use-package evil-smartparens
+  :ensure t)
+(add-hook 'smartparens-enabled-hook #'evil-smartparens-mode)
+
+;; No evil indent on open line since it interferes with haskell-mode
+(setq-default evil-auto-indent nil)
+
+;;; Misc -----------------------------------------------------------------------
+
+(use-package evil-magit
+  :ensure t)
+
+;; Fix $PATH
+(use-package exec-path-from-shell
+  :ensure t)
+(exec-path-from-shell-initialize)
+
+;; Theme
+(use-package monokai-theme
+  :ensure t)
+(load-theme 'monokai)
+
+;;; Modes ----------------------------------------------------------------------
 
 ;; Nix
-
 (require 'nix-mode)
 
 ;; Haskell
 (custom-set-variables
-  '(haskell-stylish-on-save t)
-)
+  '(haskell-stylish-on-save t))
 
 (add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
 
@@ -98,40 +111,33 @@
 (add-to-list 'grep-find-ignored-files "*.js_dyn_o")
 (add-to-list 'grep-find-ignored-directories "*.jsexe")
 
-;; intero and Haskell-mode
+;; Intero
 (use-package intero
   :ensure t)
-(setq intero-blacklist '("~/code/herculus/hexl/client/"))
+(setq intero-blacklist '("~/code/herculus/hexl/client"))
 (add-hook 'haskell-mode-hook 'intero-mode-blacklist)
 
-(require 'haskell-mode)
-(define-key haskell-mode-map [f12] 'intero-devel-reload)
-
 ;; PureScript
-; (use-package purescript-mode
-;   :ensure t
-;   :pin emacs-pe)
-;
-; (use-package psc-ide
-;   :ensure t)
+(use-package purescript-mode
+  :ensure t
+  :pin emacs-pe)
 
-; (add-hook 'purescript-mode-hook
-;           (lambda ()
-;             (psc-ide-mode)
-;             (haskell-indentation-mode)
-;             (company-mode)))
+(use-package psc-ide
+  :ensure t
+  :load-path "~/code/psc-ide-emacs")
+(setq psc-ide-use-npm-bin t)
 
-; (use-package flycheck-purescript
-;   :ensure t)
-; (eval-after-load 'flycheck
-;   '(flycheck-purescript-setup))
+(add-hook 'purescript-mode-hook
+          (lambda ()
+            (psc-ide-mode)
+            (turn-on-purescript-indentation)
+            (flycheck-mode)
+            (company-mode)))
 
 ;; Idris
-
 (use-package idris-mode :ensure t)
 
 ;; Elm
-
 (use-package elm-mode
   :ensure t)
 
@@ -142,10 +148,14 @@
 ;; CSS/SCSS
 (setq-default css-indent-offset 2)
 
+;;; General editor niceties ----------------------------------------------------
+ 
+(setq tab-width 2)
+(setq c-basic-offset 2)
+
 ;; Disable whitespace mode
 (setq prelude-whitespace nil)
 
-;;; General editor niceties
 (global-linum-mode t)
 (column-number-mode t)
 (setq-default indent-tabs-mode nil)
@@ -171,8 +181,20 @@
   (when (string= "hide" command)
     (turn-on-fci-mode)))
 
-;; No evil indent on open line since it interferes with haskell-mode
-(setq-default evil-auto-indent nil)
+;;; Font -----------------------------------------------------------------------
+
+(when (window-system)
+  (set-default-font "Fira Code"))
+
+;; To prevent bug where bold face is narrower than regular
+(set-face-attribute 'default nil :family "Fira Code")
+(set-face-attribute 'default nil :height 115)
+(set-face-attribute 'default nil :weight 'regular)
+(set-face-attribute 'bold nil :family "Fira Code")
+(set-face-attribute 'bold nil :height 117)
+(set-face-attribute 'bold nil :weight 'bold)
+
+;; -----------------------------------------------------------------------------
 
 (provide 'init)
 ;;; init.el ends here
